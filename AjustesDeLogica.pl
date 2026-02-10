71307cb69e589660210fdd348fea163b05be7fd3
Gonzalo Dip <dipgonzalo2@gmail.com>
2026-02-10 06:44:06 -0300

    Vamos a agregar el tiempo de carga del pedidio como variable de ajuste y la posible reduccion de tiempo estimado s/ prioidad

--- AjustesDeLogica.pl
+++ AjustesDeLogica.pl
@@ -25,3 +25,23 @@ ajustar_prioridad(alta, si, maxima).
 ajustar_prioridad(media, si, alta).
 ajustar_prioridad(baja, si, media).
 ajustar_prioridad(P, no, P).
+
+
+
+% ===============================
+% TIEMPO SEGUN HORA DE CARGA Y VEHICULO (AJUSTE DE HORARIO)
+% ===============================
+
+ajuste_horario(moto, antes_16, mismo_dia).
+ajuste_horario(moto, despues_16, dia_siguiente).
+
+ajuste_horario(camioneta, _, uno_a_dos_dias).
+ajuste_horario(camion, _, dentro_semana).
+
+% ===============================
+% REDUCCION DE TIEMPO POR PRIORIDAD
+% ===============================
+
+reducir_tiempo(mismo_dia, maxima, inmediato).
+reducir_tiempo(dia_siguiente, maxima, mismo_dia).
+reducir_tiempo(T, _, T).
